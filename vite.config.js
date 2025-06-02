dfa82f4d66d32bddb4b380d85c29eb7a930b9c63
skara <mekokoro1@mail.ru>
2025-06-02 20:37:07 +0300

    обавлен base в vite.config.js для GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,11 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
+  base: '/Group_project_praktika/', // 👈 обязательно для GitHub Pages
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)), 
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
